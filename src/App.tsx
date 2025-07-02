28baa3783506878334afd6ee7c61a3fc3562bd3f
Nataliia Smalchenko <137941820+nataliia-smalchenko@users.noreply.github.com>
2025-07-02 09:44:59 +0300

    add test ping

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { config } from "./utils/config";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState("Please, click the button");
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <h1>Hello, world!</h1>
+        <button
+          onClick={async () => {
+            try {
+              const response = await fetch(config.apiUrl);
+              const data = await response.json();
+              setMessage(data[0].message);
+            } catch (error) {
+              console.log(error);
+              setMessage("Error connection to API");
+            }
+          }}
+        >
+          Click
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <p>{message}</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
